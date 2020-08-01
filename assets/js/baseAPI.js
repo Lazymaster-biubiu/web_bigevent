06607db3de2265c6410506798df0c92e51f5d722
yangyang <18896806289@139.com>
2020-08-01 14:40:39 +0800

    首页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,11 +1,35 @@
 // 设置路径(测试)
 var baseURL = "http://ajax.frontend.itheima.net";
+
 // 设置路径(生产)
-var baseURL = "http://ajax.frontend.itheima.net";
+// var baseURL = "http://ajax.frontend.itheima.net";
+
 // 拦截/过滤每一次ajax请求，配置每次请求所需的参数
 $.ajaxPrefilter(function (options) {
-    // console.log(options); 未拼接前路径
-    options.url = baseURL + options.url;
-    // console.log(options); 拼接后路径
+    //发送请请求前先拼接路径
+    options.url = baseURL + options.url;// 根路径 + 请求路径
 
+
+    //统一为有权限的接口设置请求头
+    //路径有my的即为有权限接口
+    if(options.url.indexOf('/my/') !== -1) {
+
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+
+    // 全局统一挂载 complete 回调函数
+    options.complete = function(res) {
+        var data = res.responseJSON;
+        if(data.status == 1 && data.message == '身份验证失败!') {
+            //验证失败删除token
+            localStorage.removeItem('token');
+            //页面跳转
+            location.href = '/login.html';
+        }
+    }
+
+    
 })
---
+++ assets/js/index.js
@@ -0,0 +1,74 @@
+$(function () {
+    // 调用封装的获取用户信息
+    grtUserInfo()
+
+
+    var layer = layui.layer//引入layer变量
+    //退出按钮点击事件
+    $('#btnLogout').on('click', function () {
+        //提示用户是否确认退出
+        layer.confirm('是否确认退出?', {
+            icon: 3,
+            title: '提示'
+        }, function (index) {
+            //先关闭提示框
+            layer.close(index);
+            //再删除本地token
+            localStorage.removeItem('token');
+            //然后跳转页面
+            location.href = '/login.html';
+        });
+
+    })
+
+
+}) //入口函数
+
+
+// 获取用户的基本信息封装
+function grtUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers是jQuery中专门获取请求头信息的属性
+        //headers属性区分大小写
+        //所有的有权限的接口都需设置请求头，所以封装到baseAPI中
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+            // token可能会失效，需重新登陆
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败')
+            }
+            //调用renderUser封装函数，渲染用户头像
+            renderUser(res.data);
+        }
+
+        //不论成功还是失败都会调用complete回调函数，也封装到baseAPI中
+        
+    })
+
+}
+
+
+//渲染用户头像
+function renderUser(user) {
+    // 1.获取用户名称
+    var name = user.nickname || user.username;
+    //2.设置欢迎的文本
+    $('#welcom').html('欢迎&nbsp;&nbsp;' + name)
+    // 3.按需求渲染用户的头像
+    if (user.user_pic !== null) {
+        //渲染图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+
+    } else {
+        //渲染文本头像
+        var first = name[0].toUPperCase() //获取的文本名字首字母大写
+        $('.text-avatar').html(first).show()
+
+
+    }
+}
