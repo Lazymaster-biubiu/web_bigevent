868c8ccb2cfc71ce4afd69fb6d39cfba6e5dbd62
yangyang <18896806289@139.com>
2020-08-02 10:18:22 +0800

    修改个人中心资料

--- assets/js/index.js
+++ assets/js/index.js
@@ -75,27 +75,4 @@ function renderUser(user) {
 
     }
     
-
-
-
-
-
-
-    //    //获取用户名称
-    //    var name = user.nickname || user.username
-
-    //    if (user.user_pic !== null) {
-    //        //渲染用户头像
-    //        $('.layui-nav-img').attr('src', user.user_pic).show()
-    //        $('.text-avatar').hide()
-    //    } else {
-    //        //渲染文本头像
-    //        $('.layui-nav-img').hide()
-    //        var first = name[0].toUpperCase()
-    //        $('.text-avatar').html(first).show()
-    //    }
-    //    //设置欢迎的文本
-    //    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
-    //    //按需渲染用户的头像
-    
 }
